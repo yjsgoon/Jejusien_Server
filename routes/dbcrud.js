f8f9b6ab56d8bc163e7051f1eaf11cb36babff2b
JiSoo Yoon <yjsgoon@naver.com>
2016-10-11 11:28:18 +0900

    stub implement routes dbcrud

--- routes/dbcrud.js
+++ routes/dbcrud.js
@@ -5,8 +5,53 @@
 var express = require('express');
 var router = express.Router();
 
+var db = require('../db');
+
+router.get('/put', function(req, res, next) {
+    db.one("INSERT INTO [ table_name ] VALUES( [ values ] ) ON CONFLICT ( [ keys ] ) DO UPDATE SET VALUE = [ value ];",
+        [ params ])
+        .then(function () {
+            res.json({
+                resultCode: 0
+            });
+        })
+        .catch(function (err) {
+            res.json({
+                resultCode: -1,
+                msg: err
+            });
+        });
+});
+
 router.get('/get', function(req, res, next) {
-    res.send('dbcrud');
+    db.any("SELECT [ * ] FROM [ table_name ] WHERE [ * ];", [ params ])
+        .then(function (data) {
+            res.json({
+                resultCode: 0//,
+                // info: data
+            })
+        })
+        .catch(function (err) {
+            res.json({
+                resultCode: -1,
+                msg: err
+            })
+        });
+});
+
+router.get('/delete', function(req, res, next) {
+    db.one("DELETE FROM [ table_name ] WHERE [ * ];", [ params ])
+        .then(function (data) {
+            res.json({
+                resultCode: 0
+            })
+        })
+        .catch(function (err) {
+            res.json({
+                resultCode: -1,
+                msg: err
+            })
+        });
 });
 
 module.exports = router;
