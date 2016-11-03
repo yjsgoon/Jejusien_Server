429cece63b92c572aec98cd9de7adcb27c136ebf
JiSoo <yjsgoon@naver.com>
2016-11-04 05:02:41 +0900

    image test

--- routes/imagetest.js
+++ routes/imagetest.js
@@ -9,9 +9,9 @@ var request = require('request');
 
 /* 사용자의 새로운 토큰을 생성한다. */
 router.get('/', function (req, res, next) {
-    request('http://sienlogo.herokuapp.com/api/logo?url=', function (error, response, body) {
+    request('http://sienlogo.herokuapp.com/api/logo?url=https://jejusien.herokuapp.com/fileupload/get?filename=jejusien.jpg', function (error, response, body) {
         if (!error && response.statusCode == 200) {
-
+            console.log(response.body);
         }
     })
 });
